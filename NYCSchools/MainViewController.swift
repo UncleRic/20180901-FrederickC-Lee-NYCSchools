7641f70ce2c5b1debc19a75686fa72c079236a93
Frederick C. Lee <amourinetech@gmail.com>
2018-08-31 15:55:16 -0700

    Got Schools displayed.

--- NYCSchools/MainViewController.swift
+++ NYCSchools/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NYCSchools
 //
 //  Created by Frederick C. Lee on 8/31/18.
@@ -18,15 +18,22 @@ public struct School: Codable {
     let schoolName: String
 }
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     var url = URL(string: urlString)
+    var dataDict:[School] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+//    override func viewDidLayoutSubviews() {
+//        pickerView.reloadAllComponents()
+//    }
+//
     // ----------------------------------------------------------------------------------
     
     func getData() {
@@ -35,14 +42,16 @@ class ViewController: UIViewController {
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             do {
-                let dataDict = try decoder.decode([School].self, from: data!)
-                print(dataDict)
-                let schoolName = dataDict[1].schoolName
+                self.dataDict = try decoder.decode([School].self, from: data!)
+                print(self.dataDict)
+                
+                let schoolName = self.dataDict[1].schoolName
             } catch (let error) {
                 print(error.localizedDescription)
             }
             
             DispatchQueue.main.async { [weak self] in
+                
                 if let msg = error?.localizedDescription {
                     let title = "Network Error"
                     let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
@@ -50,7 +59,9 @@ class ViewController: UIViewController {
                     alertController.addAction(dismissAction)
                     self?.present(alertController, animated: true, completion: nil)
                 } else {
-                    // ... use transform() to generate a message back to the state.
+                self!.pickerView.reloadAllComponents()
+                let schoolName = self!.dataDict[1].schoolName
+                    print("School Name: \(schoolName)")
                    print("Success")
                 }
             }
@@ -61,12 +72,32 @@ class ViewController: UIViewController {
     
     @IBAction func DoSomething(_ sender: UIBarButtonItem) {
         getData()
+        
         return
     }
     
     @IBAction func exitAction(_ sender: Any) {
         exit(0)
     }
+    
+}
+
+// ============================================================================================
+
+extension MainViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataDict.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+       return dataDict[row].schoolName
+    }
+    
     
 }
 
