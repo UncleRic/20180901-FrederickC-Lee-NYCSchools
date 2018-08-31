5adf879c762454153398946102c47fc715492318
Frederick C. Lee <amourinetech@gmail.com>
2018-08-31 09:55:54 -0700

    Initial Skeleton.

--- NYCSchools/ViewController.swift
+++ NYCSchools/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
+let urlString = "https://data.cityofnewyork.us/Education/2017-DOE-High-School-Directory/s3k6-pzi2/data"
+
 class ViewController: UIViewController {
 
+    var url = URL(string: urlString)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // ----------------------------------------------------------------------------------
+    
+    @IBAction func DoSomething(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func exitAction(_ sender: Any) {
+        exit(0)
+    }
+    
 }
 
