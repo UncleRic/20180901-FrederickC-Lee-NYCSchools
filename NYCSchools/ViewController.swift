fdc8798def49446d642858f11f77f540672388ec
Frederick C. Lee <amourinetech@gmail.com>
2018-08-31 09:49:03 -0700

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Frederick C. Lee on 8/31/18.
+//  Copyright © 2018 Amourine Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
