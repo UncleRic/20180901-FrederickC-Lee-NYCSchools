02d9e09833c82b2c519a6cd6d1c4c52b13773ede
Frederick C. Lee <amourinetech@gmail.com>
2018-08-31 13:04:38 -0700

    Got Data String for Schools

--- NYCSchools/ViewController.swift
+++ NYCSchools/ViewController.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 
-let urlString = "https://data.cityofnewyork.us/Education/2017-DOE-High-School-Directory/s3k6-pzi2/data"
+let urlString = "https://data.cityofnewyork.us/resource/734v-jeq5.json"
 
 class ViewController: UIViewController {
 
     var url = URL(string: urlString)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // ----------------------------------------------------------------------------------
+    
+    func getData() {
+        URLSession.shared.dataTask(with: url!) { data, _, error in
+            let dataString = String(data: data!, encoding: .ascii)
+            
+            DispatchQueue.main.async { [weak self] in
+                if let msg = error?.localizedDescription {
+                    let title = "Network Error"
+                    let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+                    let dismissAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(dismissAction)
+                    self?.present(alertController, animated: true, completion: nil)
+                } else {
+                    // ... use transform() to generate a message back to the state.
+                   print("Success")
+                }
+            }
+            }.resume()
     }
 
     // ----------------------------------------------------------------------------------
     
     @IBAction func DoSomething(_ sender: UIBarButtonItem) {
+        getData()
+        return
     }
     
     @IBAction func exitAction(_ sender: Any) {
