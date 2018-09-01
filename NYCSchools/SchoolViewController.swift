cc0626444639f0c1168a1bd2479b583e4759b940
Frederick C. Lee <amourinetech@gmail.com>
2018-08-31 22:37:52 -0700

    Code Cleanup

--- NYCSchools/SchoolViewController.swift
+++ NYCSchools/SchoolViewController.swift
@@ -12,6 +12,7 @@ class SchoolViewController: UIViewController {
     @IBOutlet var readingLabel: UILabel!
     @IBOutlet var mathLabel: UILabel!
     @IBOutlet var writingLabel: UILabel!
+    @IBOutlet var textView: UITextView!
 
     var schoolData: School?
 
@@ -23,6 +24,7 @@ class SchoolViewController: UIViewController {
         let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
         swipeGesture.direction = .down
         view.addGestureRecognizer(swipeGesture)
+        viewLayout()
     }
 
     @objc func handleSwipeGesture() {
@@ -54,6 +56,28 @@ class SchoolViewController: UIViewController {
         attributedString = NSMutableAttributedString(attributedString: msg)
         attributedString.append(value)
         writingLabel.attributedText = attributedString
+
+        // -----------------------------------------------
+        // Description:
+        let descString = """
+        The most recent school level results for New York City on the SAT. Results are available at the school level for the graduating seniors of 2012. Records contain 2012 College-bound seniors mean SAT scores taken during SY 2012.
+        """
+        
+        msg = NSAttributedString.palatinoWithSizeAndColor(inputString: descString, size: 18.0, color: .gray)
+        textView.attributedText = msg
+    }
+
+    func viewLayout() {
+        textView.anchor(top: nil,
+                        bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                        left: view.safeAreaLayoutGuide.leftAnchor,
+                        right: view.safeAreaLayoutGuide.rightAnchor,
+                        centerYAnchor: nil,
+                        centerXAnchor: nil,
+                        paddingTop: 0,
+                        paddingLeft: 0,
+                        paddingBottom: -80,
+                        paddingRight: 0, width: 0, height: 300)
     }
 }
 
